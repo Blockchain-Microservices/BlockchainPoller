678413164cbd62e426a765e07042cfc43b51ffeb
SergeyOcheretenko <ocheretenko.s@gmail.com>
2023-02-24 13:13:08 +0200

    feat: create ConfigService

---
+++ src/config/config.service.ts
@@ -0,0 +1,53 @@
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  validateSync,
+} from 'class-validator';
+import { plainToClass } from 'class-transformer';
+import { EventPollerConfig } from './interfaces/config.interfaces';
+
+export class EnvironmentVariables {
+  @IsString()
+  @IsNotEmpty()
+  WEB3_PROVIDER: string;
+
+  @IsString()
+  @IsNotEmpty()
+  CONTRACT_ADDRESS: string;
+
+  @IsNumber()
+  @IsOptional()
+  START_BLOCK?: number;
+}
+
+@Injectable()
+export class ConfigService {
+  constructor(@Inject('ENV') private env: EnvironmentVariables) {
+    this.env = this.validateConfig();
+  }
+
+  getPollerConfig(): EventPollerConfig {
+    return {
+      provider: this.env.WEB3_PROVIDER,
+      contractAddress: this.env.CONTRACT_ADDRESS,
+      startBlock: this.env.START_BLOCK && +this.env.START_BLOCK,
+    };
+  }
+
+  private validateConfig() {
+    const validatedConfig = plainToClass(EnvironmentVariables, this.env, {
+      enableImplicitConversion: true,
+    });
+    const errors = validateSync(validatedConfig, {
+      skipMissingProperties: false,
+    });
+
+    if (errors.length > 0) {
+      throw new Error(errors.toString());
+    }
+    return validatedConfig;
+  }
+}
---
+++ src/config/interfaces/config.interfaces.ts
@@ -0,0 +1,5 @@
+export interface EventPollerConfig {
+  provider: string;
+  contractAddress: string;
+  startBlock: number;
+}
